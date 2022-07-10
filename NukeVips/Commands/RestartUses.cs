fdd3236a9dbd225beac832b1da5f1e7dfb5fef9f
Horus-X1 <leandroplaza38@gmail.com>
2022-07-10 19:05:22 -0400

    agregado RestartUses

---
+++ NukeVips/Commands/RestartUses.cs
@@ -0,0 +1,20 @@
+﻿using CommandSystem;
+using System;
+
+namespace NukeVips.Commands
+{
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    public class RestartUses : ICommand
+    {
+        public string Command => "restartuses";
+        public string[] Aliases => new string[] { "ru" };
+        public string Description => "Reiniciar usos de comandos.";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            Plugin.Usos.Clear();
+            response = "Los usos han sido reiniciados correctamente.";
+            return true;
+        }
+    }
+}
